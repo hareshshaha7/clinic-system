a4b5d261a62194a9917703007fc55e0eb73418cc
Haresh Shaha <hudaysha@tibco.com>
2021-04-05 15:07:10 +0530

    Improvised unit test case

--- src/test/java/clinic/ClinicCalendarTest.java
+++ src/test/java/clinic/ClinicCalendarTest.java
@@ -2,6 +2,7 @@ package clinic;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -17,9 +18,17 @@ class ClinicCalendarTest {
 	void AllowEntryOfAnAppointment() {
 		ClinicCalendar calendar = new ClinicCalendar();
 		calendar.addAppointment("test", "test", "haresh", "09/22/1992 08:00 AM");
+
 		List<PatientAppointment> appointments = calendar.getAppointments();
 		assertNotNull(appointments);
 		assertEquals(1, appointments.size());
+
+		PatientAppointment appointment = appointments.get(0);
+		assertEquals("test", appointment.getPatientFirstName());
+		assertEquals("test", appointment.getPatientLastName());
+		assertSame(Doctor.haresh, appointment.getDoctor());
+		assertEquals("9/22/1992 8:00 AM",
+				appointment.getAppointmenDateTime().format(DateTimeFormatter.ofPattern("M/d/yyyy h:mm a")));
 	}
 
 }
