d2850c9da7f5344c8b17e1413b89537f9ea70bb4
Haresh Shaha <haresh.shaha7@gmail.com>
2021-04-06 12:34:05 +0530

    Test case life cycle implemented

--- src/test/java/clinic/ClinicCalendarTest.java
+++ src/test/java/clinic/ClinicCalendarTest.java
@@ -6,6 +6,10 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -15,9 +19,24 @@ import org.junit.jupiter.api.Test;
  */
 class ClinicCalendarTest {
 
+	private static ClinicCalendar calendar;
+
+	@BeforeAll
+	static void SetUpBeforeAll() {
+		System.out.println(" Setting up before all ");
+
+		calendar = new ClinicCalendar(LocalDate.now());
+	}
+
+	@BeforeEach
+	void SetUpBeforeEach() {
+		System.out.println("\n Setting up before each ");
+	}
+
 	@Test
 	void AllowEntryOfAnAppointment() {
-		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		System.out.println(" Test basic appointment flow ");
+		
 		calendar.addAppointment("test", "test", "haresh", "09/22/1992 08:00 AM");
 
 		List<PatientAppointment> appointments = calendar.getAppointments();
@@ -34,23 +53,33 @@ class ClinicCalendarTest {
 
 	@Test
 	void hasAppointmentTest() {
-		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		System.out.println(" Test has appointment ");
+		
 		assertFalse(calendar.hasAppointment(LocalDate.of(1992, 9, 22)));
 
 		calendar.addAppointment("test", "test", "haresh", "09/22/1992 08:00 AM");
 		assertTrue(calendar.hasAppointment(LocalDate.of(1992, 9, 22)));
 
 	}
-	
+
 	@Test
 	void getTodaysAppointmentsTest() {
-		ClinicCalendar calendar = new ClinicCalendar(LocalDate.now());
+		System.out.println(" Test today's appointments ");
 		calendar.addAppointment("test1", "test1", "haresh", "today 08:00 AM");
 		calendar.addAppointment("test2", "test2", "sanket", "09/22/1992 12:00 AM");
 		calendar.addAppointment("test3", "test3", "akash", "today 06:00 AM");
-		
+
 		assertEquals(2, calendar.getTodaysAppointments().size());
-		
+
 	}
 
+	@AfterEach
+	void tearDownAfterEach() {
+		System.out.println(" Tear down after each\n ");
+	}
+
+	@AfterAll
+	static void tearDownAfterAll() {
+		System.out.println(" Tear down after all ");
+	}
 }
